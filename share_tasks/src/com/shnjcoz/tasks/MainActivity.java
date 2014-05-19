acf60be20263b1725cd4423b0be5913646d202c5
k_mitake <k_mitake@septeni.co.jp>
2014-05-19 17:49:16 +0900

    認証、APIでのデータ取得までが完了

--- share_tasks/src/com/shnjcoz/tasks/MainActivity.java
+++ share_tasks/src/com/shnjcoz/tasks/MainActivity.java
@@ -1,67 +1,30 @@
 package com.shnjcoz.tasks;
 
-import com.facebook.Session;
-import com.facebook.SessionState;
-import com.facebook.UiLifecycleHelper;
-
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.os.Bundle;
-import android.app.Activity;
-import android.content.Intent;
 
-public class MainActivity extends Activity {
-	private UiLifecycleHelper uiHelper;
+public class MainActivity extends FragmentActivity {
 
-    private Session.StatusCallback callback = new Session.StatusCallback() {
-        @Override
-        public void call(Session session, SessionState state, Exception exception) {
-            onSessionStateChange(session, state, exception);
-        }
-    };
-
-	private String TAG = "MainActivity";		
-			
+	private MainFragment mainFragment;
+	private String TAG = "MainActivity";	
+				
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		uiHelper = new UiLifecycleHelper(this, callback);
-		uiHelper.onCreate(savedInstanceState);
-	}
 
-	protected void onSessionStateChange(Session session, SessionState state, Exception exception) {
-		if(state.isOpened()){
-			Log.i(TAG,"Logged in ...");
-		} else if(state.isClosed()){
-			Log.i(TAG,"Logged out ...");			
+		if(savedInstanceState == null){
+			Log.i(TAG,"onCreate ...desu");
+			mainFragment = new MainFragment();
+			getSupportFragmentManager()
+			.beginTransaction()
+			.add(android.R.id.content, mainFragment)
+			.commit();
+			
+		} else {
+			Log.i(TAG,"onCreate else...desu");
+			mainFragment = (MainFragment) getSupportFragmentManager()
+							.findFragmentById(android.R.id.content);
 		}
 	}
-	@Override
-	public void onResume(){
-		super.onResume();
-		uiHelper.onResume();
-	}
-	@Override
-	public void onPause(){
-		super.onPause();
-		uiHelper.onPause();
-	}
-
-	@Override
-	public void onDestroy(){
-		super.onDestroy();
-		uiHelper.onDestroy();
-	}
-	@Override
-	public void onActivityResult(int requestCode, int resultCode, Intent data){
-		super.onActivityResult(requestCode, resultCode, data);
-		uiHelper.onActivityResult(requestCode, resultCode, data);
-	}
-	
-	@Override
-	public void onSaveInstanceState(Bundle outState){
-		super.onSaveInstanceState(outState);
-		uiHelper.onSaveInstanceState(outState);
-	}
-
 }
--- share_tasks/src/com/shnjcoz/tasks/MainFragment.java
+++ share_tasks/src/com/shnjcoz/tasks/MainFragment.java
@@ -1,22 +1,158 @@
 package com.shnjcoz.tasks;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.LoginButton;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MainFragment extends Fragment {
+	
+	private String TAG = "MainFragment";	
+	private UiLifecycleHelper uiHelper;
+	private TextView userInfoTextView;
+	final AsyncHttpClient client = new AsyncHttpClient();
+	
+	private Session.StatusCallback callback = new Session.StatusCallback() {
+	    @Override
+	    public void call(Session session, SessionState state, Exception exception) {
+	        onSessionStateChange(session, state, exception);
+	    }
+	};
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		Log.i(TAG,"onCreate...desu");
+		super.onCreate(savedInstanceState);
+		uiHelper = new UiLifecycleHelper(getActivity(), callback);
+		uiHelper.onCreate(savedInstanceState);		
+	}
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater,
 			ViewGroup container,
 			Bundle savedInstanceState){
 		
+		Log.i(TAG,"onCreateView...desu");
 		View view = inflater.inflate(R.layout.activity_main, container,false);
+		LoginButton authButton = (LoginButton) view.findViewById(R.id.authButton);
+		userInfoTextView = (TextView) view.findViewById(R.id.userInfoTextView);
+		authButton.setFragment(this);
+		authButton.setReadPermissions(Arrays.asList("user_location", "user_birthday", "user_likes"));
+		return view;			
+	}
 	
-		return view;	
-		
+	private void onSessionStateChange(Session session, SessionState state, Exception exception ){
+		if(state.isOpened()){
+			userInfoTextView.setVisibility(View.VISIBLE);
+			Log.i(TAG,"Logged in...desu");
+			Request.newMeRequest(session, new Request.GraphUserCallback() {				
+				@Override
+				public void onCompleted(GraphUser user, Response response) {
+					Log.i(TAG,"onCompleted...desu");
+					if(user != null){
+						userInfoTextView.setText(buildUserInfoDisplay(user));
+					}
+				}
+			}).executeAsync();
+			client.get("http://shnjcoz.com/sample.php", new AsyncHttpResponseHandler(){
+				@Override
+				public void onSuccess(String response){
+					Log.i(TAG,"onSuccess");
+					Log.v(TAG,response);
+				}
+				@Override
+				public void onFinish(){
+					Log.i(TAG,"onFinish");					
+				}
+				
+				@Override
+				public void onStart(){
+					Log.i(TAG,"onStart");
+				}
+			});
+			
+		}else if(state.isClosed()){
+			userInfoTextView.setVisibility(View.INVISIBLE);
+			Log.i(TAG,"Logged out...");
+		}
+	}
+	
+	@Override
+	public void onResume(){
+		super.onResume();
+		Session session = Session.getActiveSession();
+	    if (session != null &&
+	           (session.isOpened() || session.isClosed()) ) {
+	        onSessionStateChange(session, session.getState(), null);
+	    }
+		uiHelper.onResume();
+	}
+	@Override
+	public void onPause(){
+		super.onPause();
+		uiHelper.onPause();
 	}
 
+	@Override
+	public void onDestroy(){
+		super.onDestroy();
+		uiHelper.onDestroy();
+	}
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data){
+		super.onActivityResult(requestCode, resultCode, data);
+		uiHelper.onActivityResult(requestCode, resultCode, data);
+	}
+	
+	@Override
+	public void onSaveInstanceState(Bundle outState){
+		super.onSaveInstanceState(outState);
+		uiHelper.onSaveInstanceState(outState);
+	}
+	
+	private String buildUserInfoDisplay(GraphUser user){
+		Log.i(TAG,"buildUserInfoDisplay...desu");
+		StringBuffer userInfo = new StringBuffer();
+		
+		userInfo.append(String.format("Name: %S\n\n", user.getName()));
+		userInfo.append(String.format("Birthday: %S\n\n", user.getBirthday()));
+		userInfo.append(String.format("Location: %s\n\n", 
+		        user.getLocation().getProperty("name")));
+		userInfo.append(String.format("Locale: %s\n\n", 
+		        user.getProperty("locale")));
+		JSONArray languages = (JSONArray)user.getProperty("languages");
+
+		Log.i(TAG,"userInfo1: " + userInfo.toString());
+
+		if(languages.length() > 0){
+			ArrayList<String> languageNames = new ArrayList<String>();
+			for(int i = 0; i < languages.length(); i++ ){
+				JSONObject language = languages.optJSONObject(i);
+				languageNames.add(language.optString("name"));
+			}
+			userInfo.append(String.format("Languages: %s\n\n", languageNames.toString()));
+		}
+		Log.i(TAG,"userInfo2: " + userInfo.toString());
+		return userInfo.toString();		
+	}
 }
